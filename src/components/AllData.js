9600311370028569578d77742dd7f8bb91576cff
Shubh Agrawal <shubhagrawal1280@gmail.com>
2022-06-06 17:23:28 +0530

    suppliers table

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import { PRODUCT } from "../data/product";
+import { SUPPLIERS } from "../data/suppliers";
 import QueryComp from "./QueryComp";
 
 const AllData = () => {
   const [csvArray, setCsvArray] = useState([]);
   const [header, setHeader] = useState([]);
+  const [dataSet, setDataSet] = useState("product");
 
   const processCSV = (str, delim = ",") => {
     const headers = str.slice(0, str.indexOf("\n")).split(delim);
@@ -26,47 +28,95 @@ const AllData = () => {
 
   useEffect(() => {
     processCSV(PRODUCT);
-  }, []);
+    if (dataSet === "product") {
+      processCSV(PRODUCT);
+    }
+    if (dataSet === "suppliers") {
+      processCSV(SUPPLIERS);
+    }
+  }, [dataSet]);
 
   console.log(csvArray);
 
   return (
     <>
-      <QueryComp data={csvArray} header={header} />
-      <h2>Product Data</h2>
+      <QueryComp
+        data={csvArray}
+        header={header}
+        dataSet={dataSet}
+        setDataSet={setDataSet}
+      />
+      <h2>{dataSet} data</h2>
       <div>
         {csvArray.length > 0 ? (
           <>
-            <table>
-              <thead>
-                <th>productID</th>
-                <th>productName</th>
-                <th>supplierID</th>
-                <th>categoryID</th>
-                <th>quantityPerUnit</th>
-                <th>unitPrice</th>
-                <th>unitsInStock</th>
-                <th>unitsOnOrder</th>
-                <th>reorderLevel</th>
-                <th>discontinued</th>
-              </thead>
-              <tbody>
-                {csvArray.map((item, i) => (
-                  <tr key={i}>
-                    <td>{item.productID}</td>
-                    <td>{item.productName}</td>
-                    <td>{item.supplierID}</td>
-                    <td>{item.categoryID}</td>
-                    <td>{item.quantityPerUnit}</td>
-                    <td>{item.unitPrice}</td>
-                    <td>{item.unitsInStock}</td>
-                    <td>{item.unitsOnOrder}</td>
-                    <td>{item.reorderLevel}</td>
-                    <td>{item.discontinued}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {dataSet === "product" ? (
+              <table>
+                <thead>
+                  <th>productID</th>
+                  <th>productName</th>
+                  <th>supplierID</th>
+                  <th>categoryID</th>
+                  <th>quantityPerUnit</th>
+                  <th>unitPrice</th>
+                  <th>unitsInStock</th>
+                  <th>unitsOnOrder</th>
+                  <th>reorderLevel</th>
+                  <th>discontinued</th>
+                </thead>
+                <tbody>
+                  {csvArray.map((item, i) => (
+                    <tr key={i}>
+                      <td>{item.productID}</td>
+                      <td>{item.productName}</td>
+                      <td>{item.supplierID}</td>
+                      <td>{item.categoryID}</td>
+                      <td>{item.quantityPerUnit}</td>
+                      <td>{item.unitPrice}</td>
+                      <td>{item.unitsInStock}</td>
+                      <td>{item.unitsOnOrder}</td>
+                      <td>{item.reorderLevel}</td>
+                      <td>{item.discontinued}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <table>
+                <thead>
+                  <th>supplierID</th>
+                  <th>companyName</th>
+                  <th>contactName</th>
+                  <th>contactTitle</th>
+                  <th>address</th>
+                  <th>city</th>
+                  <th>region</th>
+                  <th>postalCode</th>
+                  <th>country</th>
+                  <th>phone</th>
+                  <th>fax</th>
+                  <th>homePage</th>
+                </thead>
+                <tbody>
+                  {csvArray.map((item, i) => (
+                    <tr key={i}>
+                      <td>{item.supplierID}</td>
+                      <td>{item.companyName}</td>
+                      <td>{item.contactName}</td>
+                      <td>{item.contactTitle}</td>
+                      <td>{item.address}</td>
+                      <td>{item.city}</td>
+                      <td>{item.region}</td>
+                      <td>{item.postalCode}</td>
+                      <td>{item.country}</td>
+                      <td>{item.phone}</td>
+                      <td>{item.fax}</td>
+                      <td>{item.homePage}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </>
         ) : null}
       </div>
--- src/components/QueryComp.js
+++ src/components/QueryComp.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import "../App.css";
 
-const QueryComp = ({ data, header }) => {
+const QueryComp = ({ data, header, dataSet, setDataSet }) => {
   const [query, setQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
 
@@ -36,7 +36,17 @@ const QueryComp = ({ data, header }) => {
   return (
     <>
       <h2>Enter query here</h2>
-      Select * from Product where{" "}
+      Select * from{" "}
+      <select
+        name="ds"
+        onChange={(e) => {
+          setDataSet(e.target.value);
+        }}
+      >
+        <option value="product">Product</option>{" "}
+        <option value="suppliers">Suppliers</option>{" "}
+      </select>{" "}
+      Product where{" "}
       <input
         type="text"
         onChange={(e) => {
@@ -55,36 +65,73 @@ const QueryComp = ({ data, header }) => {
       <div>
         {filteredData.length > 0 ? (
           <>
-            <table>
-              <thead>
-                <th>productID</th>
-                <th>productName</th>
-                <th>supplierID</th>
-                <th>categoryID</th>
-                <th>quantityPerUnit</th>
-                <th>unitPrice</th>
-                <th>unitsInStock</th>
-                <th>unitsOnOrder</th>
-                <th>reorderLevel</th>
-                <th>discontinued</th>
-              </thead>
-              <tbody>
-                {filteredData.map((item, i) => (
-                  <tr key={i} className="row">
-                    <td>{item.productID}</td>
-                    <td>{item.productName}</td>
-                    <td>{item.supplierID}</td>
-                    <td>{item.categoryID}</td>
-                    <td>{item.quantityPerUnit}</td>
-                    <td>{item.unitPrice}</td>
-                    <td>{item.unitsInStock}</td>
-                    <td>{item.unitsOnOrder}</td>
-                    <td>{item.reorderLevel}</td>
-                    <td>{item.discontinued}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {dataSet === "product" ? (
+              <table>
+                <thead>
+                  <th>productID</th>
+                  <th>productName</th>
+                  <th>supplierID</th>
+                  <th>categoryID</th>
+                  <th>quantityPerUnit</th>
+                  <th>unitPrice</th>
+                  <th>unitsInStock</th>
+                  <th>unitsOnOrder</th>
+                  <th>reorderLevel</th>
+                  <th>discontinued</th>
+                </thead>
+                <tbody>
+                  {filteredData.map((item, i) => (
+                    <tr key={i}>
+                      <td>{item.productID}</td>
+                      <td>{item.productName}</td>
+                      <td>{item.supplierID}</td>
+                      <td>{item.categoryID}</td>
+                      <td>{item.quantityPerUnit}</td>
+                      <td>{item.unitPrice}</td>
+                      <td>{item.unitsInStock}</td>
+                      <td>{item.unitsOnOrder}</td>
+                      <td>{item.reorderLevel}</td>
+                      <td>{item.discontinued}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <table>
+                <thead>
+                  <th>supplierID</th>
+                  <th>companyName</th>
+                  <th>contactName</th>
+                  <th>contactTitle</th>
+                  <th>address</th>
+                  <th>city</th>
+                  <th>region</th>
+                  <th>postalCode</th>
+                  <th>country</th>
+                  <th>phone</th>
+                  <th>fax</th>
+                  <th>homePage</th>
+                </thead>
+                <tbody>
+                  {filteredData.map((item, i) => (
+                    <tr key={i}>
+                      <td>{item.supplierID}</td>
+                      <td>{item.companyName}</td>
+                      <td>{item.contactName}</td>
+                      <td>{item.contactTitle}</td>
+                      <td>{item.address}</td>
+                      <td>{item.city}</td>
+                      <td>{item.region}</td>
+                      <td>{item.postalCode}</td>
+                      <td>{item.country}</td>
+                      <td>{item.phone}</td>
+                      <td>{item.fax}</td>
+                      <td>{item.homePage}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </>
         ) : null}
       </div>
